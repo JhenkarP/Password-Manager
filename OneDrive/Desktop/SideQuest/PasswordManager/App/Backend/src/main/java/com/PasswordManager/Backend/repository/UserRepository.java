fd2799dcc327dfba09bf6340f9230283fbb31b2a
Kanything <jhenkar.p2@gmail.com>
2025-06-29 21:01:01 +0530

    Backend

---
+++ OneDrive/Desktop/SideQuest/PasswordManager/App/Backend/src/main/java/com/PasswordManager/Backend/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.PasswordManager.Backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.PasswordManager.Backend.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    @EntityGraph(attributePaths = "credentials")
+    Optional<User> findWithCredentialsByUsername(String username);
+}
---
+++ OneDrive/Desktop/SideQuest/PasswordManager/App/Backend/src/main/java/com/PasswordManager/Backend/service/UserService.java
@@ -0,0 +1,43 @@
+package com.PasswordManager.Backend.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import com.PasswordManager.Backend.model.User;
+import com.PasswordManager.Backend.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> findUserWithCredentials(String username) {
+        return userRepository.findWithCredentialsByUsername(username);
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        userRepository.delete(user);
+    }
+}
